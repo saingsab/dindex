7685fcdc7388b4da80265783e1ecade3fa7d3597
Saing Sab (Ayoung) <saing.sab@gmail.com>
2022-06-01 14:30:19 +0700

    access oraclize from config

--- src/getPrice.js
+++ src/getPrice.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const { ethers } = require("ethers") 
+// const AddressList = require("./config/address.json");
+const OraclizeContract = require("../data/tokenList.json");
+
 const provider = new ethers.providers.JsonRpcProvider(process.env.URL.toString().trim());
 const aggregatorV3InterfaceABI = [{ "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "description", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint80", "name": "_roundId", "type": "uint80" }], "name": "getRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "latestRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "version", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }]
 
-const addr = "0xF9680D99D6C9589e2a93a78A04A279e509205945"
-const priceFeed = new ethers.Contract(addr, aggregatorV3InterfaceABI, provider)
+const priceFeed = new ethers.Contract(OraclizeContract.tokens[0].oraclize, aggregatorV3InterfaceABI, provider);
     priceFeed.latestRoundData()
         .then((roundData) => {
             // Do something with roundData
             console.log("Latest Round Data", roundData.answer.toString());
-        })
+        });
