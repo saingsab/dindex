66d0328592d0432316462a0b63e9a6a47d283afd
Saing Sab (Ayoung) <saing.sab@gmail.com>
2022-06-03 17:21:23 +0700

    Update getPrice.js

--- src/getPrice.js
+++ src/getPrice.js
@@ -7,7 +7,7 @@ const { bignumber } = require("mathjs");
 const provider = new ethers.providers.JsonRpcProvider(process.env.URL.toString().trim());
 const aggregatorV3InterfaceABI = [{ "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "description", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint80", "name": "_roundId", "type": "uint80" }], "name": "getRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "latestRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "version", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }]
 
-const dataFeed = async (_totalUSD) => {
+const dataFeed = async (_totalUSD, _balancePerAsset) => {
     /* 
         1. Get Total amount in USD 
         2. Allocatoin price according the percentage 
@@ -22,8 +22,14 @@ const dataFeed = async (_totalUSD) => {
         const cdFeed = new ethers.Contract(OraclizeContract.tokens[i].oraclize, aggregatorV3InterfaceABI, provider); 
         let latestPrice = await cdFeed.latestRoundData();
         // Ethers Math
-        let amount = ethers.utils.formatUnits(allocateInUSD, 6).toString();
+        // let amount = ethers.utils.formatUnits(allocateInUSD, 6).toString();
         let amount2 = ethers.utils.formatUnits(latestPrice.answer.toString(), 8).toString() ;
+        // If Returnbalance > 3%  Current Balance Sell else buy
+        if () {
+
+        } else {
+
+        }
 
         // Rebalance HERE
         console.log( Math.divide(allocateInUSD, amount2));
@@ -37,6 +43,7 @@ const getBananceFromContract = async () => {
     // Send to OracleiseAllocation
     let i = 0;
     let _balance = 0;
+    let _balancePerasset = [];
     while ( i < OraclizeContract.tokens.length) {
         let asset = OraclizeContract.tokens[i];
         let balance = await Rebalance.getBalance(asset);
@@ -51,6 +58,7 @@ const getBananceFromContract = async () => {
         i++;
     }
     // Retun in total Balance
+    // Return Blance Perasst in array
     return _balance;
 }
 
