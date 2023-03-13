4485ab9d6c0991687486988296f19385f88dc10f
Saing Sab (Ayoung) <saing.sab@gmail.com>
2023-03-13 11:00:50 +0700

    adding sigleswap sol

---
+++ contracts/singleswap.sol
@@ -0,0 +1,56 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IERC20 {
+	function totalSupply() external view returns (uint);
+	function balanceOf(address account) external view returns (uint);
+	function transfer(address recipient, uint amount) external returns (bool);
+	function allowance(address owner, address spender) external view returns (uint);
+	function approve(address spender, uint amount) external returns (bool);
+	function transferFrom(address sender, address recipient, uint amount) external returns (bool);
+	event Transfer(address indexed from, address indexed to, uint value);
+	event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+interface IUniswapV2Router {
+  function getAmountsOut(uint256 amountIn, address[] memory path) external view returns (uint256[] memory amounts);
+  function swapExactTokensForTokens(uint256 amountIn, uint256 amountOutMin, address[] calldata path, address to, uint256 deadline) external returns (uint256[] memory amounts);
+}
+
+interface IUniswapV2Pair {
+  function token0() external view returns (address);
+  function token1() external view returns (address);
+  function swap(uint256 amount0Out,	uint256 amount1Out,	address to,	bytes calldata data) external;
+}
+
+contract Takeo is Ownable {
+    function swap(address router, address _tokenIn, address _tokenOut, uint256 _amount) private {
+		IERC20(_tokenIn).approve(router, _amount);
+		address[] memory path;
+		path = new address[](2);
+		path[0] = _tokenIn;
+		path[1] = _tokenOut;
+		uint deadline = block.timestamp + 300;
+		IUniswapV2Router(router).swapExactTokensForTokens(_amount, 1, path, address(this), deadline);
+	}
+
+    function rock( address _router, address  _token1, address _token2, uint256 _amount) external onlyOwner {
+        swap(_router, _token1, _token2,_amount);
+    }
+
+    function getBalance (address _tokenContractAddress) external view  returns (uint256) {
+		uint balance = IERC20(_tokenContractAddress).balanceOf(address(this));
+		return balance;
+	}
+	
+	function withdrawEth() external onlyOwner {
+		payable(msg.sender).transfer(address(this).balance);
+	}
+
+	function withdrawTokens(address tokenAddress) external onlyOwner {
+		IERC20 token = IERC20(tokenAddress);
+		token.transfer(msg.sender, token.balanceOf(address(this)));
+	}
+}
